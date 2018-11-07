e8224c6cc45452034485ea037c8ce505e31f413c
[cai.bowen] <admin@caibowen.net>
2018-11-07 17:50:04 +0800

    bugfix

--- src/utils/dva.js
+++ src/utils/dva.js
@@ -1,3 +1,4 @@
+import Taro from '@tarojs/taro';
 import { create } from 'dva-core';
 import { createLogger } from 'redux-logger';
 import createLoading from 'dva-loading';
@@ -11,6 +12,11 @@ function createApp(opt) {
   // opt.onAction = [createLogger()];
   app = create(opt);
   app.use(createLoading({}));
+
+  // 适配支付宝小程序
+  if (Taro.getEnv() === Taro.ENV_TYPE.ALIPAY) {
+    global = {};
+  }
 
   if (!global.registered) opt.models.forEach(model => app.model(model));
   global.registered = true;
